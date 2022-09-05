dc0423e05942c2d3e6a6a154b7027cd6b9e724d4
zubetcha <zuhye5@gmail.com>
2022-09-05 22:12:23 +0900

    :lipstick: add Tags Icon

--- src/components/Elements/Icon/index.tsx
+++ src/components/Elements/Icon/index.tsx
@@ -1,5 +1,4 @@
 import classes from './Icon.module.scss';
-import classNames from 'classnames';
 import { IconRoleUnionType, IconProps } from './Icon.types';
 
 import {
@@ -15,6 +14,7 @@ import {
 	RiMenuFoldLine,
 	RiMenuUnfoldLine,
 	RiCodeSSlashLine,
+	RiHashtag,
 } from 'react-icons/ri';
 import {
 	SiTypescript,
@@ -26,100 +26,24 @@ import {
 } from 'react-icons/si';
 
 export const Icon = ({ role, size, onClick }: IconProps) => {
+	const props = { className: classes[size], onClick, id: role };
 	const icons: { [key: string]: JSX.Element } = {
-		github: (
-			<IoLogoGithub
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		twitter: (
-			<IoLogoTwitter
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		linkedIn: (
-			<IoLogoLinkedin
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		email: (
-			<MdOutlineEmail
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		'menu-fold': (
-			<RiMenuFoldLine
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		'menu-unfold': (
-			<RiMenuUnfoldLine
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		javascript: (
-			<SiJavascript
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		html: (
-			<SiHtml5
-				className={classNames(classes[size], classes.stroke)}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		css: (
-			<SiCss3
-				className={classNames(classes[size], classes.stroke)}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		typescript: (
-			<SiTypescript
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		react: (
-			<SiReact className={classes[size]} onClick={onClick} id={role} />
-		),
-		graphql: (
-			<SiGraphql className={classes[size]} onClick={onClick} id={role} />
-		),
-		about: (
-			<RiCodeSSlashLine
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		search: (
-			<IoIosSearch
-				className={classes[size]}
-				onClick={onClick}
-				id={role}
-			/>
-		),
-		home: (
-			<IoMdHome className={classes[size]} onClick={onClick} id={role} />
-		),
+		github: <IoLogoGithub {...props} />,
+		twitter: <IoLogoTwitter {...props} />,
+		linkedIn: <IoLogoLinkedin {...props} />,
+		email: <MdOutlineEmail {...props} />,
+		'menu-fold': <RiMenuFoldLine {...props} />,
+		'menu-unfold': <RiMenuUnfoldLine {...props} />,
+		javascript: <SiJavascript {...props} />,
+		html: <SiHtml5 {...props} />,
+		css: <SiCss3 {...props} />,
+		typescript: <SiTypescript {...props} />,
+		react: <SiReact {...props} />,
+		graphql: <SiGraphql {...props} />,
+		about: <RiCodeSSlashLine {...props} />,
+		search: <IoIosSearch {...props} />,
+		home: <IoMdHome {...props} />,
+		tags: <RiHashtag {...props} />,
 	};
 
 	return icons[role];
