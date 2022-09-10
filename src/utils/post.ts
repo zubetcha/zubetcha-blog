0e8e6ec59e153982a0ece9327c0d7429ab2d4216
zubetcha <zuhye5@gmail.com>
2022-09-10 22:33:52 +0900

    :heavy_plus_sign: add remark-breaks

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,6 +1,25 @@
 import fs from 'fs';
+import path from 'path';
+import { serialize } from 'next-mdx-remote/serialize';
 import { sync } from 'glob';
 
-console.log(`${process.cwd()}/posts`);
+import toc from 'remark-toc';
+import remarkGfm from 'remark-gfm';
 
-export const getAllPosts = async () => {};
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+const postsDir = path.join(process.cwd(), 'posts');
+
+export const getAllPosts = async () => {
+	const fileNames = fs.readdirSync(postsDir);
+};
+
+export const parseMdx = async (source: string) => {
+	return serialize(source, {
+		parseFrontmatter: true,
+		mdxOptions: {
+			remarkPlugins: [toc, remarkGfm],
+			rehypePlugins: [rehypeAutolinkHeadings],
+		},
+	});
+};
