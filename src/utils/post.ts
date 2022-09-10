c6489248d320757c0c569c86a647459ce9bb72ab
zubetcha <zuhye5@gmail.com>
2022-09-10 22:51:08 +0900

    :sparkles: function parseMdx (parse mdx from markdown)

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -3,10 +3,14 @@ import path from 'path';
 import { serialize } from 'next-mdx-remote/serialize';
 import { sync } from 'glob';
 
-import toc from 'remark-toc';
+import remarkToc from 'remark-toc';
 import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
+import remarkMath from 'remark-math';
 
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeKatex from 'rehype-katex';
+import rehypePrism from 'rehype-prism-plus/.';
 
 const postsDir = path.join(process.cwd(), 'posts');
 
@@ -18,8 +22,8 @@ export const parseMdx = async (source: string) => {
 	return serialize(source, {
 		parseFrontmatter: true,
 		mdxOptions: {
-			remarkPlugins: [toc, remarkGfm],
-			rehypePlugins: [rehypeAutolinkHeadings],
+			remarkPlugins: [remarkToc, remarkGfm, remarkBreaks, remarkMath],
+			rehypePlugins: [rehypeAutolinkHeadings, rehypeKatex, rehypePrism],
 		},
 	});
 };
