ac3b378fb550736a92959a2a94bc6eb406af04d4
zubetcha <zuhye5@gmail.com>
2022-09-27 01:50:14 +0900

    :lipstick: Select component done

--- src/components/Elements/Icon/Icon.types.tsx
+++ src/components/Elements/Icon/Icon.types.tsx
@@ -1,23 +1,24 @@
 export interface IconProps {
-  role: IconRoleUnionType;
-  size: 'large' | 'medium' | 'small';
-  onClick?: (e: React.MouseEvent<SVGElement>) => void;
+	role: IconRoleUnionType;
+	size: 'large' | 'medium' | 'small';
+	onClick?: (e: React.MouseEvent<SVGElement>) => void;
 }
 
 export type IconRoleUnionType =
-  | 'github'
-  | 'twitter'
-  | 'linkedIn'
-  | 'email'
-  | 'menu-fold'
-  | 'menu-unfold'
-  | 'about'
-  | 'html'
-  | 'css'
-  | 'javascript'
-  | 'typescript'
-  | 'react'
-  | 'graphql'
-  | 'about'
-  | 'search'
-  | 'home';
+	| 'github'
+	| 'twitter'
+	| 'linkedIn'
+	| 'email'
+	| 'menu-fold'
+	| 'menu-unfold'
+	| 'about'
+	| 'html'
+	| 'css'
+	| 'javascript'
+	| 'typescript'
+	| 'react'
+	| 'graphql'
+	| 'about'
+	| 'search'
+	| 'home'
+	| 'dropdown';
