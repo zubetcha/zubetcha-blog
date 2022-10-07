55278a913f57d6eb57b491371033df2c9594cc5b
zubetcha <zuhye5@gmail.com>
2022-10-08 01:26:45 +0900

    :green_heart: build error fix

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -1,2 +1,5 @@
-export * from './Post/PostContainer';
-export * from './PostList/PostListContainer';
+import { PostContainer as _PostContainer } from './Post/PostContainer';
+import { PostListContainer as _PostListContainer } from './PostList/PostListContainer';
+
+export const PostContainer: any = _PostContainer;
+export const PostListContainer: any = _PostListContainer;
