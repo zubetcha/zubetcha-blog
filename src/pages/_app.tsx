f85c4b7b21ff86650c8134e88a4992a4272a84d8
zubetcha <zuhye5@gmail.com>
2022-10-25 23:46:12 +0900

    :wrench: ga setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import { useEffect } from 'react';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+import * as gtag from '@utils/gtag';
 import { ThemeProvider } from '@context/theme';
 import { ExpandedProvider } from '@context/expanded';
 import { PageLayout } from '@components/index';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
 	useEffect(() => {
 		const setVhProperty = () => {
 			let vh = window.innerHeight * 0.01;
@@ -21,6 +24,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 			window.removeEventListener('resize', setVhProperty);
 		};
 	}, []);
+
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		router.events.on('hashChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+			router.events.off('hashChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 
 	return (
 		<>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '@utils/gtag';
 
 export default function Document() {
 	return (
@@ -94,6 +95,23 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
+				<script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+					}}
+				/>
 			</body>
 		</Html>
 	);
