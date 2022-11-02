a88829f619261802cf1fd3c60ecb7a9997f1536f
zubetcha <zuhye5@gmail.com>
2022-11-02 21:53:20 +0900

    :lipstick: developing button component

--- src/components/Elements/Button/index.tsx
+++ src/components/Elements/Button/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import classes from './Button.module.scss';
+import { Icon } from '../Icon/Icon';
+import { IconRoleUnionType } from '@type/element';
 
 interface Props {
 	onClick: () => void;
+	leftIcon?: IconRoleUnionType;
+	rightIcon?: IconRoleUnionType;
+	children: JSX.Element;
 }
-export const Button = () => {
-	return <button>Button</button>;
+export const Button = ({ onClick, children, leftIcon, rightIcon }: Props) => {
+	return (
+		<button className={classes.button} onClick={onClick}>
+			{leftIcon && <Icon role={leftIcon} />}
+			{children}
+			{rightIcon && <Icon role={rightIcon} />}
+		</button>
+	);
 };
