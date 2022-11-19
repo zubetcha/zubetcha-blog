f91f260ad24420b6ba3e082ee2fbbbf0fd6f1fab
zubetcha <zuhye5@gmail.com>
2022-11-19 11:17:39 +0900

    :lipstick: resume 수정

--- src/containers/Resume/index.tsx
+++ src/containers/Resume/index.tsx
@@ -12,8 +12,9 @@ export const ResumeContainer = () => {
 			<div className={classes['flex-column-16']}>
 				<h1>안녕하세요, 정주혜입니다.</h1>
 				<div>
-					지식을 공유하고 기록하는 것을 좋아합니다. 안정적인 코드를 작성하고,
-					유연하면서도 사용하기 쉬운 컴포넌트를 설계하기 위해 노력하고 있습니다.
+					Nextjs, React-query, Recoil을 주로 사용하며 개발하고 있습니다. <br />
+					지식을 공유하고 기록하는 것을 좋아하며, 안정적인 코드를 작성하고
+					유연하면서도 사용하기 쉬운 컴포넌트를 설계하는 일에 관심이 많습니다.
 				</div>
 				<div className={classes['item-wrapper']}>
 					<div className={classes['item-row']}>
@@ -52,17 +53,22 @@ export const ResumeContainer = () => {
 					content={{
 						title: '젠틀에너지',
 						period: '2022.03 - 현재',
-						description: '스마트 팩토리 솔루션 제공',
-						tech: 'Nextjs / React-query / Redux-toolkit / Typescript / Echarts / Turborepo',
+						description:
+							'디자인 시스템 / 웹 솔루션 / 모노레포 / 어드민 / 백오피스',
+						tech: 'Nextjs / React-query / Redux-toolkit / GraphQL / Recoil / Typescript / SCSS / Echarts / Turborepo / Storybook',
 					}}
 				>
-					<TaskSection title='디자인 시스템 구축'>
+					<TaskSection title='디자인 시스템 개발'>
 						<ul>
-							<li>공통 컴포넌트 인터페이스 설계 및 개발</li>
-							<li>UI 테스트를 위한 스토리북 환경 구성</li>
+							<li>
+								네이밍과 유연함에 중점을 둔 공통 컴포넌트 인터페이스 설계 및
+								개발
+							</li>
+							<li>UI 테스트를 위한 Storybook 프로젝트 구성</li>
+							<li>설계 과정 및 고민했던 점들을 문서로 기록하여 공유</li>
 						</ul>
 					</TaskSection>
-					<TaskSection title='B2B 대시보드 솔루션'>
+					<TaskSection title='Factoroid 솔루션'>
 						<ul>
 							<li>
 								React v17 환경에서 react-query의 useQuery를 사용해 다수의 API
@@ -74,10 +80,7 @@ export const ResumeContainer = () => {
 									렌더링 최적화
 								</a>
 							</li>
-							<li>
-								Echarts 라이브러리를 사용하여 실시간 차트, 데이터 시각화 및
-								대시보드 개발
-							</li>
+							<li>실시간 차트, 데이터 시각화 및 대시보드 개발</li>
 							<li>
 								Echarts 라이브러리의 트리쉐이킹 적용을 통해{' '}
 								<a
@@ -90,7 +93,8 @@ export const ResumeContainer = () => {
 							</li>
 							<li>
 								다국어 페이지를 개발하며 로케일별 번역본 json 스키마 추출 및
-								자동 완성 객체 생성 함수를 개발하여 휴먼 에러 발생 가능성 개선
+								자동 완성이 가능한 객체 생성 함수를 개발하여 휴먼 에러 발생
+								가능성 개선
 							</li>
 						</ul>
 					</TaskSection>
@@ -110,39 +114,24 @@ export const ResumeContainer = () => {
 							</li>
 						</ul>
 					</TaskSection>
-				</SubSection>
-				<SubSection
-					content={{
-						title: '무인양품',
-						period: '2018.03 - 2021.10',
-						description: '',
-					}}
-				>
-					<TaskSection title='생활잡화 상품 부문 MD'>
+					<TaskSection title='어드민 페이지 개발'>
 						<ul>
-							<li>판매 및 수입 계획, 예산 계획 수립</li>
-							<li>오프라인 채널 재고 컨트롤</li>
-							<li>프로모션 상품 기획</li>
+							<li>
+								계정, 고객사의 공장, 설비 및 부착된 센서를 관리하는 MES 및 ERP
+								서비스
+							</li>
+							<li>
+								클릭 이벤트와 지도 인스턴스 초기화 로직을 분리하여 카카오맵
+								렌더링 개선
+							</li>
 						</ul>
 					</TaskSection>
-				</SubSection>
-			</MainSection>
-			<MainSection title='Projects'>
-				<SubSection
-					content={{
-						title: 'MeetMeet',
-						period: '2022.08 - 2022.09',
-						description: '사내 회의 예약 관리 서비스',
-						tech: 'Nextjs / GraphQL / Recoil / Typescript',
-						link: (
-							<a href='https://github.com/zubetcha/MeetMeet' target='_blank'>
-								https://github.com/zubetcha/MeetMeet
-							</a>
-						),
-					}}
-				>
-					<TaskSection title='담당'>
+					<TaskSection title='사내 회의 예약 관리 백오피스 서비스 개발'>
 						<ul>
+							<li>
+								엑셀 시트로 회의를 예약해야 하는 불편함을 해소하고자 자발적으로
+								자체 기획 및 디자인하여 개발
+							</li>
 							<li>
 								PWA 적용 및 Firebase Cloud Messaging 서비스를 이용하여{' '}
 								<a
@@ -153,7 +142,9 @@ export const ResumeContainer = () => {
 								</a>
 							</li>
 							<li>SSE 방식을 이용하여 실시간 알림 메시지 수신 기능 개발</li>
-							<li>이미지 HEIC 확장자 변환 및 사이즈 압축 기능 개발</li>
+							<li>
+								이미지 HEIC 확장자 변환 기능을 개발하여 크로스 플랫폼 지원
+							</li>
 							<li>
 								유관부서와 QA 진행 및 개선사항 적용하여 사내 백오피스 툴 도입
 								완료
@@ -164,12 +155,14 @@ export const ResumeContainer = () => {
 						</ul>
 					</TaskSection>
 				</SubSection>
+			</MainSection>
+			<MainSection title='Projects'>
 				<SubSection
 					content={{
 						title: '블로그',
 						period: '2022.09 - 현재',
 						description: '파일 시스템 기반 개인 블로그',
-						tech: 'Nextjs / Typescript',
+						tech: 'Nextjs / Typescript / SCSS',
 						link: (
 							<a
 								href='https://github.com/zubetcha/zubetcha-blog'
@@ -180,7 +173,7 @@ export const ResumeContainer = () => {
 						),
 					}}
 				>
-					<TaskSection title='담당'>
+					<TaskSection title='SEO 최적화'>
 						<ul>
 							<li>
 								getStaticPaths와 getStaticProps를 사용하여 포스트 개수에 따라
@@ -207,19 +200,19 @@ export const ResumeContainer = () => {
 						),
 					}}
 				>
-					<TaskSection title='담당'>
+					<TaskSection title='CI/CD 및 최적화'>
 						<ul>
 							<li>Github Actions을 이용한 CI/CD 구축을 통해 DX 향상</li>
 							<li>
 								React 최적화 API 및 폰트 서브셋 제작을 통해 성능 최적화
-								(Lighthouse 퍼포먼스 점수 기준 79점 → 93점 상승)
+								(Lighthouse 퍼포먼스 점수 기준 79점 → 93점)
 							</li>
 						</ul>
 					</TaskSection>
 				</SubSection>
 			</MainSection>
 			<MainSection title='Activities'>
-				<TaskSection title='스파르타 코딩클럽 온라인 부트캠프 항해99 React 매니저'>
+				<TaskSection title='스파르타 코딩클럽 항해99 React 매니저'>
 					<ul>
 						<li>
 							스파르타 코딩클럽 항해99 (9기, 10기){' '}
@@ -250,6 +243,14 @@ export const ResumeContainer = () => {
 						</li>
 					</ul>
 				</TaskSection>
+				<TaskSection title='개발 외 업무 경험'>
+					<ul>
+						<li>
+							무인양품 상품팀 생활잡화 파트 MD (대리){' '}
+							<Period>(2018.03 - 2021.10)</Period>
+						</li>
+					</ul>
+				</TaskSection>
 			</MainSection>
 		</div>
 	);
@@ -259,15 +260,6 @@ interface SectionProps {
 	title: string;
 	children: React.ReactNode;
 }
-
-const MainSection = ({ title, children }: SectionProps) => {
-	return (
-		<div className={classes['flex-column-24']}>
-			<h2>{title}</h2>
-			{children}
-		</div>
-	);
-};
 
 interface SubSectionProps {
 	content: {
@@ -279,6 +271,15 @@ interface SubSectionProps {
 	};
 	children: React.ReactNode;
 }
+
+const MainSection = ({ title, children }: SectionProps) => {
+	return (
+		<div className={classes['flex-column-24']}>
+			<h2>{title}</h2>
+			{children}
+		</div>
+	);
+};
 
 const SubSection = ({ content, children }: SubSectionProps) => {
 	const { title, period, description, tech, link } = content;
@@ -292,7 +293,7 @@ const SubSection = ({ content, children }: SubSectionProps) => {
 				{description && (
 					<div className={classes['item-wrapper']}>
 						{link && <Item type='Github' content={link} />}
-						{tech && <Item type='Teck Stack' content={tech} />}
+						{tech && <Item type='Tech Stack' content={tech} />}
 						<Item type='Description' content={description} />
 					</div>
 				)}
